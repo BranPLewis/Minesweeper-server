8dedd04bf514689b7b6741282dcb7d3a28389bf8
Brandon Lewis <118430641+BranPLewis@users.noreply.github.com>
2025-11-18 16:50:27 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const PORT = process.env.PORT || 9091;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static("public"));
